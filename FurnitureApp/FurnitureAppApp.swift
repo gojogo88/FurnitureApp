0f8881fbd5d11521ecb025b077ec9a15f1f223f6
Jonathan Go <gojonathan.dev@gmail.com>
2022-03-01 10:34:37 +0100

    Initial Commit

---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Jonathan Go on 01.03.22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
